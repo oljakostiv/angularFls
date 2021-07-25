70679513885db266b5ea94e527eebf3797734d62
oljakostiv <oljakostivv@gmail.com>
2021-07-25 18:23:34 +0300

    new work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: '<h1>{{title}}</h1>',
+  styles: ['body{background: silver;}']
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'angularFls';
