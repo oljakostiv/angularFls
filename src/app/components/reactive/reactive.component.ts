f047d37c9383e619b8eeb8b98b663423e4228dbb
oljakostiv <oljakostivv@gmail.com>
2021-08-03 16:43:21 +0300

    new work

--- src/app/components/reactive/reactive.component.ts
+++ src/app/components/reactive/reactive.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
   styleUrls: ['./reactive.component.scss']
 })
+
 export class ReactiveComponent implements OnInit {
 
+  myForm: FormGroup = new FormGroup({user: new FormControl('vasya')});
   constructor() { }
 
   ngOnInit(): void {
