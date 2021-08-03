a5fd3efba62153710892d65fc5613758fbb01f82
oljakostiv <oljakostivv@gmail.com>
2021-08-03 15:59:02 +0300

    new work

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,6 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {IUser} from "../../models/i-user";
-import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-user',
@@ -12,13 +11,13 @@ export class UserComponent implements OnInit {
   @Input()
   user: IUser;
 
-  constructor(private router: Router) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
 
-  goToDetails(tref: HTMLFormElement) {
-    console.log(this.user)
-    this.router.navigate(['users', this.user.id], {state: this.user})
-  }
+  // goToDetails(tref: HTMLFormElement) {
+  //   console.log(this.user)
+  //   this.router.navigate(['users', this.user.id], {state: this.user})
+  // }
 }
--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {IUser} from "../../models/i-user";
 import {UserService} from "../../services/user.service";
 import {Router} from "@angular/router";
@@ -12,10 +12,18 @@ export class UsersComponent implements OnInit {
 
  users: IUser[];
 
+ @Input()
+ user: IUser
+
   constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
     this.userService.getUsers().subscribe(value => this.users = value)
+  }
+
+  goToDetails(tref: HTMLFormElement) {
+    console.log(this.user)
+    this.router.navigate(['users', this.user.id], {state: this.user})
   }
 
 }
